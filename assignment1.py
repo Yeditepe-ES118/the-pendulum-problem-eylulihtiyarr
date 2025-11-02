455dfbfd5963b8d02417e101532b24a23f758b7b
eylulihtiyarr <eylul.ihtiarsaliali@std.yeditepe.edu.tr>
2025-11-02 20:20:43 +0300

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,15 +1,1 @@
-import math
 
-def find_period(L0, L1):
-    g = 9.81  # yerçekimi ivmesi
-    for L in range(L0, L1 + 1):
-        T = 2 * math.pi * math.sqrt(L / g)
-        print("When L =", round(float(L), 1), "m, T =", round(T, 1), "s")
-
-    # ilk ve son uzunluklar için periyotlar
-    T0 = 2 * math.pi * math.sqrt(L0 / g)
-    T1 = 2 * math.pi * math.sqrt(L1 / g)
-    return T0, T1
-
-# örnek çalıştırma
-find_period(2, 10)
